3a8627e971149614524cfdb943213c26a4e29c54
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-06-30 22:15:05 -0400

    feat: update styling, tune viz

--- src/pages/viz.tsx
+++ src/pages/viz.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next";
 import React, { useState } from "react";
+import { css } from "emotion";
 
 import ScheduleViz from "../components/vega/ScheduleViz";
 import WordCloudViz from "../components/vega/WordCloudViz";
 import { Page } from "../components/site";
+import { Center, VSpace } from "../components/layout";
 
 const VizPage: NextPage = () => {
   const [schedHoverID, setSchedHoverID] = useState(null);
@@ -11,12 +13,27 @@ const VizPage: NextPage = () => {
 
   return (
     <Page>
-      <h1>What's Everyone Talking About at JuliaCon?</h1>
+      <Center>
+        <h1
+          className={css`
+            font-weight: bold;
+            font-size: 2rem;
+          `}
+        >
+          What's Everyone Talking About at JuliaCon?
+        </h1>
+      </Center>
 
-      <p>
-        JuliaCon 2020 talks by time and virtual room, or topic - hover over a
-        point or word to explore!
-      </p>
+      <VSpace />
+
+      <Center>
+        <p>
+          JuliaCon 2020 talks by time and virtual room, or topic - hover over a
+          point or word to explore!
+        </p>
+      </Center>
+
+      <VSpace />
 
       <div className="my5">
         <ScheduleViz wcHover={wcHoverIDs} setHover={setSchedHoverID} />
