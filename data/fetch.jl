c1baddf51d118d5a5fd638493a5e2f2bf097b98d
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-06-30 21:18:07 -0400

    feat: viz page, minimal functionality

--- data/fetch.jl
+++ data/fetch.jl
@@ -1,5 +1,7 @@
 using HTTP, JSON
 
+include("viz_data_preprocess.jl")
+
 for resource in ("rooms", "talks", "speakers")
   resp = HTTP.get("https://pretalx.com/api/events/juliacon2020/$(resource)?limit=500")
   data = JSON.parse(String(resp.body))
@@ -8,6 +10,9 @@ for resource in ("rooms", "talks", "speakers")
   end
   open("$(resource).json", "w") do io
     JSON.print(io, data["results"], 1)
+    if resource == "talks"
+      viz_data_preprocess(data["results"])
+    end
   end
   @info "Got $(length(data["results"])) $(resource)"
 end
---
+++ src/components/vega/ScheduleViz.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import vegaEmbed from "vega-embed";
+import spec from "../../assets/vega/schedule";
+import data from "../../assets/vega/sched_viz_data";
+
+export default class ScheduleViz extends Component {
+  constructor(props) {
+    super(props);
+    this.view = null;
+  }
+
+  updateView(v) {
+    this.view = v;
+  }
+
+  componentDidMount() {
+    vegaEmbed("#schedule", spec, {
+      mode: "vega",
+      actions: false,
+      renderer: "svg",
+      loader: { target: "_blank" },
+    }).then((res) => {
+      try {
+        console.log(data);
+        console.log(res);
+        res.view
+          .insert("talks", data)
+          .resize()
+          .runAsync()
+          .then((view) => {
+            this.updateView(view);
+            console.log(view);
+            // update the global state with the current mouseover
+            view.addEventListener("mouseover", (name, value) => {
+              if (value && value.datum.index) {
+                this.props.setHover(value.datum.index);
+              } else {
+                this.props.setHover(null);
+              }
+            });
+
+            view.addEventListener("mouseout", (name, value) => {
+              this.props.setHover(null);
+            });
+          });
+      } catch (error) {
+        console.log("OH NO - The Schedule Viz Broke!");
+        console.log(error);
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    var update = this.props.wcHover ? this.props.wcHover : [];
+    this.view.signal("hoverIDs", update).run();
+  }
+
+  render() {
+    return <div id="schedule"></div>;
+  }
+}
