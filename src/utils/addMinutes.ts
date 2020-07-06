29c6c7bdbd6253972658313d9bbf02abf2146729
Momir Milutinovic <milutinm462@gmail.com>
2020-07-06 23:20:49 +0200

    Documented addMinutes

--- src/utils/addMinutes.ts
+++ src/utils/addMinutes.ts
@@ -1,5 +1,7 @@
 import { isNonNull } from "./null";
 
+// Adds some number of minutes to a time passed as a string in ISO format
+// Null values are treated as zeroes
 export function addMinutes(time: string, minutes: number | null | undefined) {
   minutes = isNonNull(minutes) ? minutes : 0;
   let newTime = new Date(time);
