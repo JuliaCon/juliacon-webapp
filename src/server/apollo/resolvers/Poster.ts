4dc619f75b9122295bd82751720633e37d583c06
Cris Moen <cris.moen@runtosolve.com>
2020-07-20 17:25:29 -0400

    add pdflink and revise speaker call

--- src/server/apollo/resolvers/Poster.ts
+++ src/server/apollo/resolvers/Poster.ts
@@ -7,34 +7,13 @@ import { PosterResolvers } from "./__types__";
 
 export const Poster: PosterResolvers = {
   
-  id: async (root, _args, { dataSources }) => {
-    return dataSources.pretalx.getPoster(root.posterId);
-  },
-
-  title: async (root, _args, { dataSources }) => {
-    return dataSources.pretalx.getPoster(root.posterId);
-  },
-
-  abstract: async (root, _args, { dataSources }) => {
-    return dataSources.pretalx.getPoster(root.posterId);
-  },
-
-  description: async (root, _args, { dataSources }) => {
-    return dataSources.pretalx.getPoster(root.posterId);
-  },
-
-  day: (root) => {
-    const date = new Date(root.startTime);
-    return format(date, `yyyy-MM-dd`);
-  },
-
   pdflink: async (root, _args, { dataSources }) => {
-    return dataSources.pretalx.getPoster(root.posterId);
+    return null;
   },
   
   speakers: async (root, _args, { dataSources }) => {
     const speakers = await asyncMap(root.speakerIds, (speakerId) =>
-      dataSources.pretalx.getPoster(speakerId)
+      dataSources.pretalx.getSpeaker(speakerId)
     );
     return speakers.filter(isNonNull);
   },
