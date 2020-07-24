8b9845791d08b56757311bc62625d28dd5853498
Momir Milutinovic <milutinm462@gmail.com>
2020-07-24 18:21:22 +0200

    Sponosor bar works on big screen except on viz

--- src/components/site/MainContentContainer.tsx
+++ src/components/site/MainContentContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Sidebar } from "./Sidebar";
 export const MainContentContainer: React.FC = ({ children }) => {
   return (
-    <div className={"sdff"}>
+    <div className={"container"}>
       <div className={"content"}>{children}</div>
       <Sidebar />
     </div>
