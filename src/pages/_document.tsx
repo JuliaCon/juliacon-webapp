a985300b0f28a7f869f880c4590e596a74ed5793
Travis DePrato <travisdeprato@gmail.com>
2020-07-24 09:49:05 -0700

    Add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,9 @@ class AppDocument extends Document {
   render() {
     return (
       <html>
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{ __html: gaScript }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -39,3 +41,13 @@ class AppDocument extends Document {
 }
 
 export default AppDocument;
+
+const gaScript = `
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-28835595-3', 'auto');
+          ga('send', 'pageview');
+`;
