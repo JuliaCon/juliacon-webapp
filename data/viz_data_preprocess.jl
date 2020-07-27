0ac1f3b83313cf2e62b6c55c54582fc2941572ba
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-07-27 13:59:05 -0400

    fix: change from time to date filtering

--- data/viz_data_preprocess.jl
+++ data/viz_data_preprocess.jl
@@ -1,11 +1,12 @@
 using TextAnalysis
+using Dates
 
 # data wrangling and nlp for viz
 function viz_data_preprocess(data)
     talks = []
     index = 1
     for talk in data
-        if talk["duration"] < 100
+        if Date(talk["slot"]["start"][1:10]) >= Date("2020-07-29")
             push!(talks, parse_talk(talk, index))
             index += 1
         end
