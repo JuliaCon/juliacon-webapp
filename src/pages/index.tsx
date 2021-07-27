201ddc426b44eca0e1c3a264c9c9bf09e98ddc0f
Avik Sengupta <avik@sengupta.net>
2021-07-27 16:47:44 +0100

    No Stickermule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,21 +65,6 @@ export const IndexPage: NextPage = () => {
               >
                 Julia Bonfire store
               </ExternalLink>
-              . JuliaCon is also proud to be sponsored by{" "}
-              <ExternalLink
-                href={"http://stickermule.com/juliacon20?utm_source=juliacon"}
-              >
-                Sticker Mule
-              </ExternalLink>
-              ! Head to their site to get 10 Julia stickers for only one 1 Euro
-              (plus free shipping!). You can find the Julia Language logo{" "}
-              <ExternalLink
-                href={
-                  "https://github.com/JuliaLang/julia-logo-graphics/blob/master/images/julia-logo-color.png"
-                }
-              >
-                here
-              </ExternalLink>
               .
             </p>
           </VSpaceBetween>
