22d5398988b759383434f746e8f6620f25e469f6
Ranjan Anantharaman <ranjan.anantharaman@gmail.com>
2023-07-26 10:11:03 -0400

    Update Page.tsx

--- src/components/site/Page.tsx
+++ src/components/site/Page.tsx
@@ -51,6 +51,6 @@ export const Page: React.FC<PageProps> = ({
 
 const twitterHandle = "JuliaConOrg";
 const metaDescription =
-  `JuliaCon 2022 (with JuMP-dev) is online and everywhere! ` +
-  `Join us to learn about the cool and exciting things happening within the Julia & JuMP ecosystems!`;
+  `JuliaCon 2023 (with JuMP-dev, SciMLCon and ASE60)` +
+  `Join us to learn about the cool and exciting things happening within the Julia, JuMP and SciML ecosystems. Also, celebrate ASE60 with us!`;
 const previewImage = "https://juliacon.org/assets/2021/img/world_2800.png";
